ab1feb485334cb368e30ad698faf901f5ed88982
huda0209 <huda030209@gmail.com>
2020-10-09 21:23:22 +0900

    [change]引数からfsを受け取るように変更 見やすく整形

--- src/fileManeger.js
+++ src/fileManeger.js
@@ -1,34 +1,44 @@
 class fileManeger {
-    constructor(fs) {
-        this.fs = fs;
+    
+    //fsを引数から受け取る　相対パスをmain.jsがあるディレクトリからに
+    async write1 (content,fs){
+        try{
+            fs.writeFileSync('./config/write-1.json', JSON.stringify(content, null, "\t"),'utf8');
+            console.log(`write1 complete`);
+        }catch(e){
+            console.log(`write1 fail\n${e}\n-------------------------------\n`);
+        };
     };
-    //fsをコンストラクタから受け取る　ファイルの相対パスをmain.jsがあるディレクトリからにする
-    async write1 (content){
-        const fs = this.fs;
-        try{fs.writeFileSync('./config/write-1.json', JSON.stringify(content, null, "\t"),'utf8');
-        console.log(`write1 complete`)
-        }catch(e){console.log(`write1 fail\n-------------------------------\n${e}`)}
-    };
-    //fsをメソッド内でインポートする　ファイルの相対パスをmain.jsがあるディレクトリからにする
+
+    //fsをメソッド内でインポート　相対パスをmain.jsがあるディレクトリからに
     async write2 (content) {
         const fs = require('fs');
-        try{fs.writeFileSync('./config/write-2.json', JSON.stringify(content, null, "\t"),'utf8');
-        console.log(`write2 complete`)
-        }catch(e){console.log(`write2 fail\n-------------------------------\n${e}`)}
+        try{
+            fs.writeFileSync('./config/write-2.json', JSON.stringify(content, null, "\t"),'utf8');
+            console.log(`write2 complete`);
+        }catch(e){
+            console.log(`write2 fail\n${e}\n-------------------------------\n`);
+        };
     };
-    //fsをコンストラクタから受け取る　ファイルの相対パスをfileManeger.jsがあるディレクトリからにする
-    async write3 (content) {
-        const fs = this.fs;
-        try{fs.writeFileSync('../config/write-3.json', JSON.stringify(content, null, "\t"),'utf8');
-        console.log(`write3 complete`)
-        }catch(e){console.log(`write3 fail\n-------------------------------\n${e}\n-------------------------------\n`)}
+
+    //fsを引数から受け取る　相対パスをfileManeger.jsがあるディレクトリからに
+    async write3 (content,fs) {
+        try{
+            fs.writeFileSync('../config/write-3.json', JSON.stringify(content, null, "\t"),'utf8');
+            console.log(`write3 complete`);
+        }catch(e){
+            console.log(`write3 fail\n${e}\n-------------------------------\n`);
+        };
     };
-    //fsをメソッド内でインポートする　ファイルの相対パスをfileManeger.jsがあるディレクトリからにする
+    //fsをメソッド内でインポート　相対パスをfileManeger.jsがあるディレクトリからに
     async write4 (content) {
         const fs = require('fs');
-        try{fs.writeFileSync('../config/write-4.json', JSON.stringify(content, null, "\t"),'utf8');
-        console.log(`write4 complete`)
-        }catch(e){console.log(`write4 fail\n-------------------------------\n${e}\n-------------------------------\n`)}
+        try{
+            fs.writeFileSync('../config/write-4.json', JSON.stringify(content, null, "\t"),'utf8');
+            console.log(`write4 complete`);
+        }catch(e){
+            console.log(`write4 fail\n${e}\n-------------------------------\n`);
+        };
     };
 };
 module.exports = fileManeger; //クラス出力
