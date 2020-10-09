593da683910d271778a9e152816ee9d548214b16
huda0209 <huda030209@gmail.com>
2020-10-09 21:21:58 +0900

    [change]見やすく整形

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const fs = require('fs'); //nodeモジュール読みこみ
 const fileManegerClass = require('./src/fileManeger.js') //クラスファイル読みこみ
-const fileManeger = new fileManegerClass(fs);  //インスタンス化
+const fileManeger = new fileManegerClass();  //インスタンス化
 
 const content = {"name" : "taro" , "age" : 30};
-fileManeger.write1(content); //fsをメソッド内でインポートする　ファイルの相対パスをmain.jsがあるディレクトリからにする
-fileManeger.write2(content); //fsをメソッド内でインポートする　ファイルの相対パスをmain.jsがあるディレクトリからにする
-fileManeger.write3(content); //fsをコンストラクタから受け取る　ファイルの相対パスをfileManeger.jsがあるディレクトリからにする
-fileManeger.write4(content); //fsをメソッド内でインポートする　ファイルの相対パスをfileManeger.jsがあるディレクトリからにする
+
+//fsを引数から受け取る　相対パスをmain.jsがあるディレクトリからに
+    fileManeger.write1(content, fs);
+
+//fsをメソッド内でインポート　相対パスをmain.jsがあるディレクトリからに
+    fileManeger.write2(content);
+
+//fsを引数から受け取る　相対パスをfileManeger.jsがあるディレクトリからに
+    fileManeger.write3(content, fs);
+
+//fsをメソッド内でインポート　相対パスをfileManeger.jsがあるディレクトリからに
+    fileManeger.write4(content);
